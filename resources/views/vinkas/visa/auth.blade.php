8ad484e924cc0918bb588e6771a4c83208675a1a
Vinoth Kannan <vinothkannan@vinkas.com>
2016-07-28 15:06:05 +0530

    material design implemented

--- resources/views/vinkas/visa/auth.blade.php
+++ resources/views/vinkas/visa/auth.blade.php
@@ -1,18 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+@extends('material.skeleton')
 
-  <title>
-    {{ getEnv('APP_NAME')}}
-  </title>
-
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-  <link rel="stylesheet" href="/vinkas/firebase/auth.css" />
-</head>
-<body>
+@section('body')
   <header id="header">
     <div class="text-center">
       <a href="/" class="logo"><img src="/images/logo.png" /></a>
@@ -89,3 +77,4 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 </body>
 </html>
+@endsection
