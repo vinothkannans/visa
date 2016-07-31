591fd7a848596365a2b90dc5f5ba09096626f894
Vinoth Kannan <vinothkannan@vinkas.com>
2016-07-31 13:08:03 +0530

    HomeController added

---
+++ vinkas/visa/Http/Controllers/Controller.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Vinkas\Visa\Http\Controllers;
+
+use Vinkas\Visa\Traits\Redirects;
+use App\Http\Controllers\Controller as BaseController;
+
+/**
+ *
+ */
+class Controller extends BaseController
+{
+
+  use Redirects;
+
+  const CLIENT = "client";
+
+}
